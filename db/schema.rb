87e1687346071b0a6ddcdb3b117ff013c0b6c716
jake0988 <jake098877@gmail.com>
2020-12-22 21:28:45 -0500

    task and group_task column for goal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_014208) do
+ActiveRecord::Schema.define(version: 2020_12_23_022649) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_014208) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "goal"
   end
 
   create_table "group_users", force: :cascade do |t|
@@ -76,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_014208) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "start"
     t.integer "user_id"
+    t.string "goal"
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
